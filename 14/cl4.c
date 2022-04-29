c7bd7826225b095488d60897fa682f18edff11b4
Aditya Choudhury <adityasubham03@gmail.com>
2022-04-29 17:17:26 +0530

    updated cl4

--- 14/cl4.c
+++ 14/cl4.c
@@ -2,11 +2,11 @@
 
 void main(){
     int a;
-    char uname[6]={'A','D','I','T','Y','A'};
+    char uname[6]={'A','D','I','T','Y','A'},cname[6];
     for (int i = 0; i < 6; i++)
     {
-        a = uname[i];
-        printf("%c",a+1);
+        cname[i]=(int)(uname[i]+1);
+        printf("%c",cname[i]);
     }
     
 
