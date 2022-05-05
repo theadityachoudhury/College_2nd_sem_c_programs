e7cf142e0ff3ee44c160cfc9ff391dae7fa62514
Aditya Choudhury <adityasubham03@gmail.com>
2022-05-05 15:20:59 +0530

    added 16

---
+++ 16/cl1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int b[2][2] = {1,2,3,4};
+    printf("%d\n",*&b);
+    printf("%d\n",&b);
+    printf("%d\n",&b+1);
+    printf("%d\n",*b);
+    printf("%d\n",*b+1);
+}
